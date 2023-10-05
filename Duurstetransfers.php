92f1f161bf77197182d3ac7d4056101f8336e783
Thijsgerjanvdl <145574932+Thijsgerjanvdl@users.noreply.github.com>
2023-10-05 13:32:31 +0200

    Add files via upload

---
+++ Duurstetransfers.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<table>
+  <tr>
+    <th>Spelernaam</th>
+    <th>Oude Club</th>
+    <th>Nieuwe club</th>
+    <th>Transfersom</th>
+  </tr>
+  <tr>
+    <td>Declan Rice</td>
+    <td>West Ham United</td>
+    <td>Arsenal</td>
+    <td>116,6 m</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Moises Caicedo</td>
+    <td>Brighton and Hove Albian</td>
+    <td>Chelsea</td>
+    <td>116 m</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Jude Bellingham</td>
+    <td>Borussia Dortmund</td>
+    <td>Real Madrid</td>
+    <td>103 m</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Harry Kane</td>
+    <td>Tottenham Hotspur</td>
+    <td>Bayern Munchen</td>
+    <td>100 m</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Randal Kolo Muani</td>
+    <td>Eintracht Frankfurt</td>
+    <td>Paris Saint Germain</td>
+    <td>95 m</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Josko Gvardiol</td>
+    <td>Red Bull Leipzig</td>
+    <td>Manchester City</td>
+    <td>90 m</td>
+  </tr>
+   <tr>
+    <td></td>
+    <td>Neymar</td>
+    <td>Paris Saint Germain</td>
+    <td>Al Hilal</td>
+    <td>90 m</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Rasmus Højlund</td>
+    <td>Atalanta Bergamo</td>
+    <td>Manchester United</td>
+    <td>75 m</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Kai Havertz</td>
+    <td>Chelsea</td>
+    <td>Arsenal</td>
+    <td>75 m</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td>Dominik Szoboszlai</td>
+    <td>Red Bull Leipzig</td>
+    <td>Liverpool</td>
+    <td>70 m</td>
+  </tr>
+</table>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 16px;  
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn {
+  background-color: green;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #ffffff;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
+</head>
+<body style="background-color:white;">
+
+<div class="navbar">
+  <div class="dropdown">
+    <button class="dropbtn">Transfers 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Duurste</a>
+      <a href="#">Meest recente</a>
+      <a href="#">Contractverlengingen</a>
+  </div>
+  </div>
+   <div class="dropdown">
+    <button class="dropbtn">Competities
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Eredivisie</a>
+      <a href="#">Keukenkampioen Divisie</a>
+      <a href="#">Premier League</a>
+    </button>
+     </div>
+     </div>
+
+   <div class="dropdown">
+    <button class="dropbtn">Topscoorders+assisters 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="Eredivsie.php">Eredivisie</a>
+      <a href="#">Keukenkampioen Divisie</a>
+      <a href="#">Premier League</a>
+    </button>
+    </div>
+  </div> 
+</div>
+<style>
+body {
+ background-image: url("achtergrond.png");
+ background-color: #cccccc;
+background-size: cover;
+</style>
+</body>
+<body>
+
+
+
+<a class="navbar-brand" href="/">      <div class="logo-image">            <img src="logo.png"  class="logo-image" >      </div></a>
+<style>
+.logo-image{
+    width: 100px;
+    height: 100px;
+    border-radius: 80%;
+    overflow: hidden;
+    margin-top: -900px;
+
+
+}
+</style>
+</body>
+</html>
